8e65e226314a270f18444f65989629c1ebfee651
Lorenzo <lorenzopica31@gmail.com>
2024-11-14 16:19:32 +0100

    added some server side function(needs fixes)

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
 import os
 import vlc
-from flask import Flask
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return render_template("index.html")
+    if __name__ == __main__:
+        app.run(debug=True)
 
+        def play_video(): data = request.get_json()
+    video_link = data['audioLink']
 
+    try:
+       
+        Instance = vlc.Instance()
+        player = Instance.media_player_new()
+        media = Instance.media_new(video_link)
+        player.set_media(media)
+
+    
+        player.video_set_spu(False)
+
+        player.play()
+        return 'Audio started'
+    except Exception as e:
+        return f'Errore durante la riproduzione: {str(e)}'
