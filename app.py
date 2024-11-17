5b0125701e0fda8585b06dcfc488d9503a834031
Lorenzo <lorenzopica31@gmail.com>
2024-11-17 16:23:29 +0100

    delted html and completly changed code for the project

--- app.py
+++ app.py
@@ -1,29 +1,11 @@
+import tkinter as tk
+import fnmatch
 import os
-import vlc
-from flask import Flask, render_template, request
+from pygame import mixer
 
-app = Flask(__name__)
-@app.route("/",method = ["POST"])
-def index():
-    return render_template("index.html")
-    if __name__ == __main__:
-        app.run(debug=True)
-        
-    @app.route("/play",method = ["POST"])
-    def play_audio(): data = request.get_json()
-    audio_link = data['audioLink']
+canvas = tk.Tk()
+canvas.title("Media-player")
+canvas.geometry("600x800")
+canvas.config(bg = 'black')
 
-    try:
-       
-        Instance = vlc.Instance()
-        player = Instance.media_player_new()
-        media = Instance.media_new(audio_link)
-        player.set_media(media)
-
-    
-        player.video_set_spu(False)
-
-        player.play()
-        return 'Audio started'
-    except Exception as e:
-    return f'Error: {str(e)}
+canvas.mainloop()
