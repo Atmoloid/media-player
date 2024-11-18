33035b4788c89a1da75c659b9b91161095e8e087
Lorenzo <lorenzopica31@gmail.com>
2024-11-18 16:18:45 +0100

    provided basic UI

--- app.py
+++ app.py
@@ -8,4 +8,9 @@ canvas.title("Media-player")
 canvas.geometry("600x800")
 canvas.config(bg = 'black')
 
+rootpath = "/home/lorenzo/Music"
+pattern = ".*mp3"
+listBox = tk.Listbox(canvas, fg="cyan", bg = "black", width = 100)
+listBox.pack(padx = 15, pady = 15)
+
 canvas.mainloop()
