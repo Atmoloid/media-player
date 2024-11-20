1ef8146359ae52264e968b55fe951d46585aae7f
Lorenzo <lorenzopica31@gmail.com>
2024-11-20 16:21:13 +0100

    added images to button and gave function to play and stop

--- app.py
+++ app.py
@@ -10,6 +10,25 @@ canvas.config(bg = 'black')
 
 rootpath = "/home/lorenzo/Music"
 pattern = "*.mp3"
+
+mixer.init()
+
+prev_img = tk.PhotoImage(file = "prev_img.png")
+stop_img = tk.PhotoImage(file = "stop_img.png")
+play_img = tk.PhotoImage(file = "play_img.png")
+next_img = tk.PhotoImage(file = "next_img.png")
+pause_img = tk.PhotoImage(file = "pause_img.png")
+
+def select():
+  label.config(text = listBox.get("anchor"))
+  mixer.music.load(rootpath + "/" + listBox.get("anchor"))
+  mixer.music.play()
+
+ 
+def stop():
+      mixer.music.stop()
+      listBox.select.clear("active")
+
 listBox = tk.Listbox(canvas, fg="cyan", bg = "black", width = 100, font = ('ds-digital', 14))
 listBox.pack(padx = 15, pady = 15)
 
@@ -20,11 +39,20 @@ label.pack(pady = 15)
 top = tk.Frame(canvas, bg="black")
 top.pack(padx = 10, pady = 5, anchor="center")
 
-prevButton = tk.Button(canvas, text="Prev")
+prevButton = tk.Button(canvas, text="Prev", image= prev_img, bg="black", borderwidth = 0)
 prevButton.pack(pady = 15, in_ = top, side = "left")
 
-stopButton = tk.Button(canvas, text="Pause")
+stopButton = tk.Button(canvas, text="Stop", image= stop_img, bg="black", borderwidth = 0, command= stop)
 stopButton.pack(pady = 15, in_ = top, side = "right")
+
+playButton = tk.Button(canvas, text="Play", image= play_img, bg="black", borderwidth = 0, command= select )
+playButton.pack(pady = 15, in_ = top, side = "right")
+
+nextButton = tk.Button(canvas, text="Next", image= next_img, bg="black", borderwidth = 0)
+nextButton.pack(pady = 15, in_ = top, side = "right")
+
+pauseButton = tk.Button(canvas, text="Pause", image= pause_img, bg="black", borderwidth = 0)
+pauseButton.pack(pady = 15, in_ = top, side = "right")
 
 for root, dirs, files in os.walk(rootpath):
   for filename in fnmatch.filter(files, pattern):
