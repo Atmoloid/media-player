a09e002121b55f7e2b58a923cff72709dc7ea968
Lorenzo <lorenzopica31@gmail.com>
2024-11-25 15:07:20 +0100

    changed next_button positioning

--- app.py
+++ app.py
@@ -84,11 +84,11 @@ stopButton.pack(pady = 15, in_ = top, side = "right")
 playButton = tk.Button(canvas, text="Play", image= play_img, bg="black", borderwidth = 0, command= select )
 playButton.pack(pady = 15, in_ = top, side = "right")
 
-nextButton = tk.Button(canvas, text="Next", image= next_img, bg="black", borderwidth = 0, command= play_next)
-nextButton.pack(pady = 15, in_ = top, side = "right")
-
 pauseButton = tk.Button(canvas, text="Pause", image= pause_img, bg="black", borderwidth = 0, command= pause_song)
 pauseButton.pack(pady = 15, in_ = top, side = "right")
+
+nextButton = tk.Button(canvas, text="Next", image= next_img, bg="black", borderwidth = 0, command= play_next)
+nextButton.pack(pady = 15, in_ = top, side = "right")
 
 for root, dirs, files in os.walk(rootpath):
   for filename in fnmatch.filter(files, pattern):
